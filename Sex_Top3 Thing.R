1b19031c86e8fb738b1c25805d9f2500c9bf90c0
Reagan-Chiaverini <reaganrc@colostate.edu>
2022-12-07 11:24:37 -0700

    Adding Age Class Figure

--- Sex_Top3 Thing.R
+++ Sex_Top3 Thing.R
@@ -1,10 +1,19 @@
 library(tidyverse)
+library(ggthemes)
 new <- lifts %>% 
   group_by(date, sex2) %>% 
   summarize(avg_top3 = mean(total_three),
-            date = date)
+            date = date) %>%
+  distinct(sex2, .keep_all = TRUE)
+
 new %>% 
   ggplot(aes(x = date, y = avg_top3, color = sex2)) + 
   labs(x = "Date", y = "Total Average 3 Lifts") +
-  geom_line() +
-  facet_wrap(~sex2)
+  geom_point() +
+  geom_jitter() +
+  stat_smooth() +
+  facet_wrap(~sex2) +
+  theme_few() +
+  labs(y = "Average of 3 Lifts",
+       title = "Average of 3 Lifts Over Time by Sex") +
+    guides(color = guide_legend(title = "Sex"))
