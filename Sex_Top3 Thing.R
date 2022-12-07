69c26f359180d345caf79c2a81a3480e7971b6c4
Cydney Jardine <cydney.jardine@colostate.edu>
2022-12-07 10:38:35 -0700

    Sex plot done

--- Sex_Top3 Thing.R
+++ Sex_Top3 Thing.R
@@ -2,9 +2,15 @@ library(tidyverse)
 new <- lifts %>% 
   group_by(date, sex2) %>% 
   summarize(avg_top3 = mean(total_three),
-            date = date)
+            date = date) %>%
+  distinct(sex2, .keep_all = TRUE)
+
+
+
+
 new %>% 
   ggplot(aes(x = date, y = avg_top3, color = sex2)) + 
   labs(x = "Date", y = "Total Average 3 Lifts") +
-  geom_line() +
+  geom_point() +
+  stat_smooth() +
   facet_wrap(~sex2)
